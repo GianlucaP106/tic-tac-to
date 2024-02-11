7d209a1663b0a2930251bcfa20bd1a4f278bc252
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-02-11 13:21:30 -0500

    tic tac to

---
+++ src/auth/nextauth.ts
@@ -0,0 +1,31 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import { Adapter } from "next-auth/adapters"
+import type { NextAuthOptions } from 'next-auth'
+import { prisma } from "../../prisma/prisma"
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        })
+    ]
+}
+
+export const handler = NextAuth(authOptions)
